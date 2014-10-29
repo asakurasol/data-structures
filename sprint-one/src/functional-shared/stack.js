0c78c7d0202ec01834cae17185cf2622ae3d13b2
asakurasol <mamamia1734@gmail.com>
2014-10-29 15:49:21 -0700

    up to functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    counter: 0
+  };
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  },
+  pop: function() {
+    if (this.counter > 0) {
+      var results = this.storage[this.counter];
+      delete this.storage[this.counter];
+      this.counter--;
+      return results;
+    }
+  },
+  size: function() {
+    return this.counter;
+  }
+};
+
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
+  var heads = 0;
+  var tails = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    heads++;
+    storage[heads] = value;
   };
 
   someInstance.dequeue = function(){
+    πif ((heads - tails) > 0) {
+      tails++;
+      var results = storage[tails];
+      delete storage[tails];
+      return results;
+    }
   };
 
   someInstance.size = function(){
+    return heads - tails;
   };
 
   return someInstance;
