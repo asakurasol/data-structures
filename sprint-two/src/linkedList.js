89dcdff743653c562841abe6209f419bc5d59252
asakurasol <mamamia1734@gmail.com>
2014-10-30 15:48:05 -0700

    added up to tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,43 @@
 var makeLinkedList = function(){
   var list = {};
+  var id = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    id++;
+    list[id] = makeNode(value);
+
+    if (list.head === null) {
+      list.head = list[id];
+    }
+
+    if (list.tail !== null) {
+      list.tail.next = list[id];
+    }
+
+    list.tail = list[id];
   };
 
   list.removeHead = function(){
+    var newHead = list.head.next;
+    var oldValue = list.head.value;
+    delete list.head;
+    list.head = newHead;
+    return oldValue;
   };
 
   list.contains = function(target){
+    var recurse = function(node) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null){
+        return false;
+      } else {
+        return recurse(node.next);
+      }
+    };
+    return recurse(list.head);
   };
 
   return list;
@@ -29,3 +57,6 @@ var makeNode = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//Add to Tail : O(1)
+//removeHead: O(1)
+//contains: O(n)
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,9 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
+  newTree.id = 0;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,11 +13,27 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.id++;
+  if(this.children === undefined){
+    this.children = [];
+  }
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
-
+  if(this.value === target){
+    return true;
+  } else if(this.children) {
+    var result = false
+    for(var i = 0; i<this.children.length; i++){
+      if(this.children[i].contains(target)){
+        result = true;
+      };
+    }
+    return result
+  } else {
+    return false;
+  }
 };
 
 
