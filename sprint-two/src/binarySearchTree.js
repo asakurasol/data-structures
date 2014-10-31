d1478fe4664829593080b056352013c591ee7eea
asakurasol <mamamia1734@gmail.com>
2014-10-31 14:35:03 -0700

    working on binary

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,36 @@
-var makeBinarySearchTree = function(value){
+var MakeBinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+MakeBinarySearchTree.prototype.insert = function(value) {
+  if (value === this.value){
+    return value;
+  }
+
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = new MakeBinarySearchTree(value);;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = new MakeBinarySearchTree(value);;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+MakeBinarySearchTree.prototype.contains = function() {
+
+};
+
+MakeBinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
